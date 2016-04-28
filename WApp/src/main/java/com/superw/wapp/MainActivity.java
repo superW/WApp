3a79fabde8b1421a9828c91961db636a8b42c1d9
superW <wangyanchao000@126.com>
2016-04-28 17:11:20 +0800

    test commit text change

--- WApp/src/main/java/com/superw/wapp/MainActivity.java
+++ WApp/src/main/java/com/superw/wapp/MainActivity.java
@@ -1,25 +1,27 @@
 package com.superw.wapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.tv).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ((TextView) v).setText("success");
-            }
-        });
+        tv = (TextView) findViewById(R.id.tv);
+                tv.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        ((TextView) v).setText("success");
+                    }
+                });
 
         Button btn = (Button) findViewById(R.id.btn);
         btn.setText("click me");
@@ -27,8 +29,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "ok", Toast.LENGTH_SHORT).show();
+                tv.setText("beautiful click");
             }
         });
+
+
 
     }
 }

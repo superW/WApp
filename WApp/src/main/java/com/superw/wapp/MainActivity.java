c3c12e7f18428222e43b2418c03c760299150db9
superW <wangyanchao000@126.com>
2016-04-28 17:03:13 +0800

    test commit ok button

--- WApp/src/main/java/com/superw/wapp/MainActivity.java
+++ WApp/src/main/java/com/superw/wapp/MainActivity.java
@@ -3,7 +3,9 @@ package com.superw.wapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,8 +17,18 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.tv).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ((TextView)v).setText("success");
+                ((TextView) v).setText("success");
             }
         });
+
+        Button btn = (Button) findViewById(R.id.btn);
+        btn.setText("click me");
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "ok", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }

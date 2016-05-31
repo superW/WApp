7e42d04112234742bda885c51f6fc9e4ba483bb5
superW <wangyanchao000@126.com>
2016-05-31 17:21:06 +0800

    BaseFragment change.

--- WApp/src/main/java/com/superw/wapp/BaseFragment.java
+++ WApp/src/main/java/com/superw/wapp/BaseFragment.java
@@ -1,12 +1,32 @@
 package com.superw.wapp;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by wangyc-e on 2016/5/19.
  */
-public class BaseFragment extends Fragment {
+public abstract class BaseFragment extends Fragment {
+
+    private View mView;
+
+    public abstract int getLayoutId();
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mView = inflater.inflate(getLayoutId(), container, false);
+        return mView;
+    }
 
 
+    public <T> T getView(int id) {
+        View view = mView.findViewById(id);
+        return (T) view;
+    }
 
 }
